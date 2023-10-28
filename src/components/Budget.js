0a0579083cf18a405e82de86b30013d8e1b814ab
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2023-10-28 17:24:33 +0700

    finsih budget allocation app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,25 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, dispatch} = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+        handleBudgetState(event.target.value)
+    }
+
+    const handleBudgetState = (budgetValue) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: budgetValue,
+        });
+    };
+    return (
+        <div className='alert alert-secondary'>
+        <span>Budget: £{budget}</span>
+        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
+    );
+};
+export default Budget;
