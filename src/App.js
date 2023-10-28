781d55603181496d73b655b6d53aed4eb3dd6c3d
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2023-10-28 21:26:15 +0700

    add component currency

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -17,7 +18,7 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                <div className='row mt-3'>
+                <div className='row mt-3 '>
                     <div className='col-sm'>
                         <Budget />
                     </div>
@@ -26,6 +27,9 @@ const App = () => {
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
                     </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,24 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, dispatch} = useContext(AppContext);
+    const { budget, dispatch, expenses, currency} = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-        handleBudgetState(event.target.value)
+        const assignedBudget = event.target.value;
+        if(assignedBudget > 20000) {
+            alert("The value cannot exceed £20000");
+            return;
+        }
+        if(assignedBudget < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
+        setNewBudget(assignedBudget);
+        handleBudgetState(assignedBudget)
     }
 
     const handleBudgetState = (budgetValue) => {
@@ -16,9 +29,15 @@ const Budget = () => {
         });
     };
     return (
-        <div className='alert alert-secondary'>
-        <span>Budget: £{budget}</span>
-        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        <div className='alert alert-secondary '>
+            <span>Budget: {currency}</span>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+                style={{ width: '150px' }}
+            ></input>
         </div>
     );
 };
